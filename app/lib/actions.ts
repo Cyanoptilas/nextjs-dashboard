145a1054b231ad111042d307717a935f2120bb40
Cyanoptilas <februarys22@gmail.com>
2024-02-03 01:15:33 +0900

    ADD Invoice更新機能追加(actionにupdate追加、編集ページ追加)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,6 +18,8 @@ const FormSchema = z.object({
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
@@ -39,6 +41,28 @@ export async function createInvoice(formData: FormData) {
   // このキャッシュをクリアして、サーバーへの新しいリクエストをトリガー
   // データベースが更新されると、/dashboard/invoicesパスが再検証され、
   // 新しいデータがサーバーからフェッチされる
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+  UPDATE invoices
+  SET customer_id = ${customerId},
+      amount = ${amountInCents},
+      status = ${status}
+  WHERE id =${id}
+  `;
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
